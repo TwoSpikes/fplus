2fadb68073be6de118e69f053e13495a1cfb2b35
Two Spikes <twospikesfpg@gmail.com>
2023-01-14 11:17:05 +0300

    Implement gotoifs and labels

--- main.rs
+++ main.rs
@@ -23,7 +23,6 @@ fn cla(args: &Vec<String>) -> Result<Mode, i32> {
     }
     match args[1].as_str() {
         "sim" => {
-            println!("Simulation is about to start");
             if args.len() <= 2 {
                 println!("No source file provided");
                 usage();
@@ -130,60 +129,187 @@ fn strtoi64(x: &String) -> Option<i64> {
     Push(i64),
     PRINT,
     PLUS,
+    //gotoif
+    GIF,
 }
-fn parse(pr: &Vec<Tok>) -> Option<Vec<Op>> {
-    let mut res: Vec<Op> = vec![];
+fn parse(pr: &Vec<Tok>, filename: &String) -> Option<Vec<Op>> {
+    let mut res: Vec<Result<Op, &str>> = vec![];
+    #[derive(Debug)]
+    enum State {
+        NONE,
+        //label without definition
+        LBL,
+        //label with definition
+        FN,
+        OPER,
+    }
+    let mut state: State = State::NONE;
+    let mut labels: Vec<(&str, Option<i64>)> = Vec::new();
+    let mut main: Option<usize> = None;
     for i in pr {
         let val: &String = &i.1;
         let loc: &Loc = &i.0;
         let lin: &i64 = &loc.0;
         let ind: &i64 = &loc.1;
-        println!("val=`{}`, loc.lin={}, loc.ind={}", val, lin, ind);
+        //println!("val=`{}`, loc.lin={}, loc.ind={}", val, lin, ind);
         res.append(&mut match strtoi64(val) {
-            Some(x) => vec![
-                Op::Push(x),
-            ],
+            Some(x) => {
+                vec![
+                    Ok(Op::Push(x)),
+                ]
+            },
             None => {
+                match state {
+                    State::NONE =>
                 match val.as_str() {
                     "" => {
                         continue;
                     },
                     "+f" => vec![
-                        Op::PLUS,
+                        Ok(Op::PLUS),
                     ],
                     "putc" => vec![
-                        Op::PRINT,
+                        Ok(Op::PRINT),
                     ],
+                    "lbl" => {
+                        state = State::LBL;
+                        continue;
+                    },
+                    "impl" => {
+                        state = State::FN;
+                        continue;
+                    },
+                    ":if" => vec![
+                        Ok(Op::GIF),
+                    ],
+                    _ => vec![
+                        Err(val.as_str()),
+                    ],
+                },
+                    State::LBL => {
+                        if let "main" = &*val.as_str() {
+                            main = Some(res.len().try_into().unwrap());
+                        }
+                        labels.push((val.as_str(), None));
+                        state = State::NONE;
+                        continue;
+                    },
+                    State::FN => {
+                        let pos: usize = match labels.iter().position(|x| String::from(x.0).eq(val)) {
+                            Some(pos) => pos,
+                            None => {
+                                println!("{}:{}:{}: label declaration to implement not found: {}", filename, lin, ind, val);
+                                return None;
+                            }
+                        };
+                        labels[pos].1 = Some(res.len().try_into().unwrap());
+                        state = State::NONE;
+                        continue;
+                    }
                     _ => {
-                        println!("Unknown token: `{}`", val);
+                        println!("Unknown state of parser (debug): `{:?}`", state);
                         return None;
                     },
                 }
             }
         });
     }
-    return Some(res);
+    //to avoid not founding labels
+    labels.push(("", None));
+
+    //linking
+    println!("Linking... labels={:?}\nres={:?}", labels, res);
+    let mut linkres: Vec<Op> = Vec::new();
+    let mut ind: i64 = -1;
+    for i in res {
+        ind += 1;
+        match i {
+            //simple operation
+            Ok(x) => linkres.push(x),
+            //found label call
+            Err(x) => {
+                let mut ret: i64 = -1;
+                let mut found: bool = false;
+                //tring to find declaration
+                for j in &labels {
+                    //if found by name
+                    if String::from(j.0).eq(&String::from(x)) {
+                        found = true;
+                        match j.1 {
+                            //found defenition
+                            Some(def) => {
+                                println!("found label definition: {}", def);
+                                linkres.push(Op::Push(def));
+                            },
+                            //not found definition
+                            None => {
+                                println!("label is declared, but has no definition");
+                                return None;
+                            }
+                        }
+                    } else {
+                        ret += 1;
+                    }
+                }
+            },
+        };
+    }
+    linkres.push(Op::Push(match main {
+        Some(x) => x.try_into().unwrap(),
+        None => 0,
+    }));
+    return Some(linkres);
 }
 
-fn sim(pr: &Vec<Op>) -> i32 {
+fn sim(pr: &mut Vec<Op>, filename: &String) -> Option<i32> {
+    println!("simulating {}: {:?}", filename, pr);
     let mut stack: Vec<i64> = vec![];
-    for i in pr {
-        println!("sim: {:?}", i);
+    let main: i64 = match pr.pop() {
+        Some(x) => match x {
+            Op::Push(y) => {
+                println!("sim: debug: main label was found: {}", y);
+                y
+            },
+            _ => {
+                println!("sim: debug: main label not found");
+                return None;
+            }
+        },
+        None => return Some(0),
+    };
+    let mut ind: i64 = main - 1;
+    while ind < pr.len().try_into().unwrap() {
+        ind += 1;
+        let i: &Op = &pr[<i64 as TryInto<usize>>::try_into(ind).unwrap()];
+        println!("sim:{}: {:?}", ind, i);
         match i {
-            Op::Push(x) => stack.push(*x),
-            Op::PRINT => print!("print: {}", stack.pop().unwrap()),
+            Op::Push(x) => {
+                stack.push(*x);
+            },
+            Op::PRINT => {
+                print!("print: {}", char::from_u32(stack.pop().unwrap().try_into().unwrap()).unwrap());
+            },
             Op::PLUS => {
                 let a: i64 = stack.pop().unwrap();
                 let b: i64 = stack.pop().unwrap();
                 stack.push(a + b)
             },
+            Op::GIF => {
+                let addr: i64 = stack.pop().unwrap() - 1;
+                let cond: i64 = stack.pop().unwrap();
+                println!("sim: debug: ifgotoifing to {} if {}", addr, cond);
+                if cond != 0 {
+                    println!("sim: debug: gotoifing is true stack={:?}", stack);
+                    ind = addr.try_into().unwrap();
+                }
+            },
             _ => {
                 println!("Unknown op: {:?}", i);
-                return -1;
+                return None;
             },
         }
     }
-    return 0;
+    return Some(0);
 }
 
 fn clah(args: &Vec<String>) {
@@ -193,10 +319,10 @@ fn clah(args: &Vec<String>) {
             match mode {
                 Mode::SIM => {
                     for i in &args[2..] {
-                        let err: i32 = sim(&match parse(&lex(&match get(i) {
+                        let err: Option<i32> = sim(&mut match parse(&lex(&match get(i) {
                             Some(x) => x,
                             None => continue,
-                        })) {
+                        }), i) {
                             Some(x) => {
                                 println!("[Parsing succed]");
                                 x
@@ -205,11 +331,18 @@ fn clah(args: &Vec<String>) {
                                 println!("[Parsing failed]");
                                 continue;
                             },
-                        });
-                        if err == 0 {
-                            println!("[Simulation of `{}` succed]", i);
-                        } else {
-                            println!("[Simulation of `{}` failed with exit code {}", i, err);
+                        }, i);
+                        match err {
+                            Some(x) => {
+                                if x == 0 {
+                                    println!("[Simulation of `{}` succed]", i);
+                                } else {
+                                    println!("[Simulation of `{}` was finished with exit code {}]", i, x);
+                                }
+                            },
+                            None => {
+                                println!("[Simulation of `{}` failed", i);
+                            }
                         }
                     }
                 },
