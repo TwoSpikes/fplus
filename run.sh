928dff6bf8dffacc389a404cbf306657cf8d7eba
Two Spikes <twospikesfpg@gmail.com>
2023-04-14 11:50:08 +0300

    [run.sh] Update script

--- run.sh
+++ run.sh
@@ -4,18 +4,10 @@ set -e
 
 make install
 
-run() {
-	set from $0;
-	set all $1;
-	fplus ${all:$from};
-	unset from all;
-	return $?;
-}
-
 if [[ $1 == --in-main-rs ]] then
-	run 2 $@;
+	fplus ${@:2};
 else
-	run 1 $@;
+	fplus ${@:1};
 fi
 
 set errorcode $?;
